f2bae182583add482a248a8db6bcabc79d2ef44d
23di <yura.frolov@gmail.com>
2014-02-17 04:01:39 -0800

    Update to print from SD

--- Arduino to serial/Arduino to serial.ino
+++ Arduino to serial/Arduino to serial.ino
@@ -1,26 +1,54 @@
-#include "Adafruit_Thermal.h"
+#include "Thermal.h"
+#include "SD.h"
+#include "SPI.h"
+#include <avr/pgmspace.h>
 #include "SoftwareSerial.h"
-#include "Stream.h"
 
-/**
- * Declare which pins to communicate to the printer over
- */
-int printer_RX_Pin = 5; // зеленый провод
-int printer_TX_Pin = 6; // желтый провод
+int printer_RX_Pin = 5;  // This is the green wire
+int printer_TX_Pin = 6;  // This is the yellow wire
 
-/**
- * Инициализируем принтер
- */
-Adafruit_Thermal printer(printer_RX_Pin, printer_TX_Pin);
+File myFile;
+
+Thermal printer(printer_RX_Pin, printer_TX_Pin);
 
 void setup(){
+  Serial.begin(9600);
+
+  Serial.print("Initializing SD card...");
+  pinMode(7, OUTPUT); digitalWrite(7, LOW);
+  pinMode(10, OUTPUT);
+
+  if (!SD.begin(4)) {
+    Serial.println("initialization failed!");
+    return;
+  }
+  Serial.println("initialization done.");
+  
   printer.begin();
-  Serial.begin(9600); //Дефолтная скорость принтера
+    if (!SD.exists("3.bin"))
+  {
+    printer.println("File not exists");
+  }
+  else
+  {
+  
+  myFile = SD.open("3.bin");
+  
+  if (myFile)
+  {
+  
+  printer.printBitmap(384, 310, dynamic_cast<Stream*>(&myFile));
+  }
+  else
+  {
+    printer.println("File not exists");
+  }
+
+}
+  printer.sleep();      // Tell printer to sleep
+  printer.wake();       // MUST call wake() before printing again, even if reset
+  printer.setDefault(); // Restore printer to defaults
 }
 
-void loop(){
-  if (Serial.available() > 0){
-    printer.printBitmap(384,75,&Serial); 
-    Serial.write(1); //Печатаем все что пришло на порт
-  }
+void loop() {
 }
